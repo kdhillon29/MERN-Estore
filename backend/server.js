9b86b218ae5f1663c08cd3a48fc2e5c9ed372a5b
kan dhillon <kdhillon469@gmail.com>
2025-05-26 19:00:32 +1000

    add   mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -7,6 +10,17 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Ecommerce Store API" });
+});
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
