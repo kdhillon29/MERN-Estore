9a8a0456bdc422784a0351bb6fedd14dbed297da
kan dhillon <kdhillon469@gmail.com>
2025-05-29 09:59:36 +1000

    add coupon  route

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import connectDB from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import productRoutes from "./routes/product.route.js";
-
+import cartRoutes from "./routes/cart.routes.js";
+import couponRoutes from "./routes/coupon.route.js";
 dotenv.config();
 
 const app = express();
@@ -18,6 +19,8 @@ app.use(cookieParser());
 //routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupon", couponRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Ecommerce Store API" });
