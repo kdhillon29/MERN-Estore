385a60b0bbcef447befd54d66265c9a7d77d42c4
kan dhillon <kdhillon469@gmail.com>
2025-05-29 10:52:43 +1000

    add profile route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,7 +4,9 @@ import {
   login,
   logout,
   refreshToken,
+  getProfile,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -15,5 +17,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/refresh-token", refreshToken);
+
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
