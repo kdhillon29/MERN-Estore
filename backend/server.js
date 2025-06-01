c81381eca6ef54992a42c6dbbd28bed732d4819e
kan dhillon <kdhillon469@gmail.com>
2025-06-01 11:39:45 +1000

    add production config

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,9 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 //middleware
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
 //routes
@@ -26,9 +26,17 @@ app.use("/api/coupon", couponRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the Ecommerce Store API" });
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to the Ecommerce Store API" });
+// });
 
 app.listen(PORT, () => {
   connectDB();
