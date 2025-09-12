d7b634fb1efb63a5d39d027e19494bd8bfb01c3f
kan dhillon <kdhillon469@gmail.com>
2025-09-12 16:24:17 +1000

    update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://mern-estore-sh15.onrender.com",
+export default defineConfig((config) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: `${
+            config.mode === "production"
+              ? "https://mern-estore-sh15.onrender.com"
+              : "http://localhost:5000"
+          }`,
+        },
       },
     },
-  },
+  };
 });
