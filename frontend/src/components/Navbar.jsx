52c05734a11d336e9a5de246c9f1e1ebbd52a378
kan dhillon <kdhillon469@gmail.com>
2025-09-12 17:16:04 +1000

    Revert "update vite config"
    This reverts commit d7b634fb1efb63a5d39d027e19494bd8bfb01c3f.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,9 +23,9 @@ const Navbar = () => {
         <div className="flex flex-wrap justify-between items-center">
           <Link
             to="/"
-            className=" max-sm:text-md md:text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
+            className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
           >
-            DK Store
+            E-Commerce
           </Link>
 
           <nav className="flex flex-wrap items-center gap-4">
@@ -72,7 +72,7 @@ const Navbar = () => {
             {user ? (
               <button
                 className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-    						rounded-md flex items-center transition duration-300 ease-in-out"
+						rounded-md flex items-center transition duration-300 ease-in-out"
                 onClick={logout}
               >
                 <LogOut size={18} />
@@ -83,7 +83,6 @@ const Navbar = () => {
                 <Link
                   to={"/signup"}
                   className="bg-emerald-600 hover:bg-emerald-700 text-white py-2 px-4 
-                  max-sm:p-1 text-xs md:text-md
 									rounded-md flex items-center transition duration-300 ease-in-out"
                 >
                   <UserPlus className="mr-2" size={18} />
@@ -92,7 +91,6 @@ const Navbar = () => {
                 <Link
                   to={"/login"}
                   className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
-                  max-sm:p-1 text-xs md:text-md
 									rounded-md flex items-center transition duration-300 ease-in-out"
                 >
                   <LogIn className="mr-2" size={18} />
