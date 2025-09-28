b4132be1397e7daf9490928261960ca5cc187c48
kan dhillon <kdhillon469@gmail.com>
2025-09-28 20:40:06 +1000

    update frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 export const URL = window.location.href.includes("localhost")
   ? "http://localhost:5000"
-  : "https://mern-estore-henna.vercel.app";
+  : "https://mern-estore-backend.vercel.app";
